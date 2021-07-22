4b67e05759db5bee112da905b5b96815d5dc6c3d
Artyom Erofeev <34557625+ErofeevAA@users.noreply.github.com>
2021-07-22 16:17:23 +1000

    add draw.js

---
+++ draw.js
@@ -0,0 +1,52 @@
+class Draw {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.context = canvas.getContext("2d");
+    }
+
+    drawBackground() {}
+}
+
+class DrawMenu extends Draw {
+
+    constructor(canvas) {
+        super(canvas);
+    }
+
+    drawBackground() {
+        super.drawBackground();
+        this.context.fillStyle = "#000000";
+        this.context.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+    drawHeader(header) {
+        this.context.strokeStyle = "#FFF000";
+        this.context.textAlign = "center";
+        this.context.strokeText(header.text, header.x + header.width / 2, header.y, header.width);
+        //this.context.fillText(header.text, header.x, header.y, header.width);
+    }
+
+    drawButton(button) {
+        //this.context.strokeStyle = "#000000"
+        this.context.fillStyle = "#FFFFFF"
+        let x = button.x;
+        let y = button.y;
+        let radius = button.radius;
+        let width = button.width;
+        let height = button.height;
+        let text = button.text;
+        this.context.beginPath();
+        this.context.arc(x, y, radius,  0, 2 *Math.PI);
+        this.context.closePath();
+        this.context.fill();
+        this.context.fillRect(x, y - radius, width, height);
+        this.context.beginPath();
+        this.context.arc(x + width, y, radius,  0, 2 * Math.PI);
+        this.context.closePath();
+        this.context.fill();
+        this.context.fillStyle = "#000";
+        this.context.textAlign = "center";
+        this.context.fillText(text, x  + width / 2, y + 3);
+    }
+
+}
