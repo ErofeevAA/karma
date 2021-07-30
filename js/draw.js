6b357f28f29e6f820f2b4a3f40208a8830c4cbe9
AlikaJam <C0ttie@yandex.ru>
2021-07-31 07:47:59 +1000

    fix game

--- js/draw.js
+++ js/draw.js
@@ -25,9 +25,7 @@ class Draw {
     drawBackground() {}
     drawAbout() {}
     drawTrain() {}
-    drawParameters() {}
     drawMultiplayer() {}
-    drawGame() {}
 }
 
 class DrawMenu extends Draw {
@@ -75,21 +73,6 @@ class DrawTrain extends Draw {
         this.context.drawImage(card_shirt, 1180, 50, 100, 100);
         this.context.drawImage(from_bottom_to_top, 1180, 250, 100, 100);
         this.context.drawImage(five_or_less, 1180, 450, 100, 100);
-    }
-}
-
-class DrawParameters extends Draw {
-    constructor(canvas_about) {
-        super(canvas_about);
-    }
-
-    drawParameters() {
-        super.drawAbout();
-        this.context.globalAlpha = 0.2;
-        this.context.drawImage(sea, 0, 0, 1340, 600);
-        this.context.globalAlpha = 0.6;
-        this.context.fillStyle = '#0B0C10';
-        this.context.fillRect(300, 200, 650, 300);
     }
 }
 
--- js/model_menu.js
+++ js/model_menu.js
@@ -3,9 +3,7 @@ class ModelMenu {
     constructor() {
         this.header = new TextBlock(MenuEnum.HEADER, "text-header");
         this.btn_multiplayer = ModelMenu.createButton(MenuEnum.MULTIPLAYER, 0, 0);
-        this.btn_single = ModelMenu.createButton(MenuEnum.SINGLE_PLAYER, 0, 0);
         this.btn_train = ModelMenu.createButton(MenuEnum.TRAINING, 0, 0);
-        this.btn_parameters = ModelMenu.createButton(MenuEnum.PARAMETERS, 0, 0);
         this.btn_about = ModelMenu.createButton(MenuEnum.ABOUT, 0, 0);
     }
 
@@ -19,8 +17,6 @@ class ModelMenu {
 const MenuEnum = {
     HEADER: "It's KARMA",
     MULTIPLAYER: "Мультиплеер",
-    SINGLE_PLAYER: "Одиночная игра",
     TRAINING: "Правила",
-    PARAMETERS: "Параметры",
     ABOUT: "О нас",
 };
